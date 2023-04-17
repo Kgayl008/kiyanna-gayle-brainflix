85144ec72c83de04923989b2fa760cc9f9760428
Kiyanna Gayle <Kgayl008@fiu>
2023-04-16 20:05:23 -0400

    completed axios

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -1,151 +1,36 @@
-import './VideoList.scss'
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
+import "./VideoList.scss";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-const VideoList = ({clickHandler }) => {
-    const [ video, setVideo ] = useState([])
-    
-    useEffect(() => {
-        axios.get(`https://project-2-api.herokuapp.com/videos?api_key=a1262fa0-3078-466e-a961-bfd24f06df57`)
-        .then(response => {
-            console.log(response.data);
-            setVideo(response.data)
-        });
-    }, [setVideo]);
+const VideoList = ({ videos, selectedVideoId }) => {
+  const [filteredVideos, setFilteredVideos] = useState([]);
 
+  useEffect(() => {
+    setFilteredVideos(videos.filter(video => video.id !== selectedVideoId));
+  }, [videos, selectedVideoId]);
 
-    return(
-        <div className='videolist__section'>
-        <p className='videolist__header'>NEXT VIDEOS</p>
-        {video.length > 0 && video.map((v) => (
-            <div className='videolist__container' onClick={() => clickHandler(v.id)}>
-                <div>
-                    <img src={v.image} alt="Thumbnail of each video in the video list section." className='videolist__video' />
-                </div>
-                <div className='videolist__text'>
-                    <p className='videolist__title'>{v.title}</p>
-                    <p className='videolist__channel'>{v.channel}</p>
-                </div>
+  return (
+    <div className="videolist__section">
+      <p className="videolist__header">NEXT VIDEOS</p>
+      {filteredVideos.map((video) => (
+        <Link to={`/video/${video.id}`} key={video.id} className="videolist__links">
+          <div className="videolist__container">
+            <div>
+              <img
+                src={video.image}
+                alt="Thumbnail of each video in the video list section."
+                className="videolist__video"
+              />
             </div>
-        ))}
+            <div className="videolist__text">
+              <p className="videolist__title">{video.title}</p>
+              <p className="videolist__channel">{video.channel}</p>
+            </div>
+          </div>
+        </Link>
+      ))}
     </div>
-    )
-}
+  );
+};
 
 export default VideoList;
-
-
-
-
-
-
-
-
-
-// import './VideoList.scss'
-// import axios from 'axios';
-// import { useState, useEffect } from 'react';
-
-// function VideoList({selectedVideoId}) {
-//     const [video, setVideo] = useState(null);
-
-//     useEffect(() => {
-//         if(selectedVideoId === null){
-//             return;
-//         }
-//         axios.get(`https://week-7-plants.onrender.com/plants/${selectedVideoId}?api_key=123`)
-//             .then(response => {
-//                 console.log(response.data);
-//                 setVideo(response.data);
-//             })
-//     }, [selectedVideoId])
-    
-//     if(video === null) {
-//         return <div>Loading!!</div>
-//     }    
-    
-//     // Destructure properties from selected plant
-//     const {image, title, channel} = video;
-
-//     return(
-//         <div className='videolist__section'>
-//         <p className='videolist__header'>NEXT VIDEOS</p>
-//             <div className='videolist__container' >
-//                 <div>
-//                     <img src={image} alt="Thumbnail of each video in the video list section." className='videolist__video' />
-//                 </div>
-//                 <div className='videolist__text'>
-//                     <p className='videolist__title'>{title}</p>
-//                     <p className='videolist__channel'>{channel}</p>
-//                 </div>
-//             </div>
-//     </div>
-//     )
-// }
-
-// export default VideoList;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import Data from '../../data/videos.json'
-// import './VideoList.scss'
-// import axios from 'axios';
-
-// const VideoList = ({clickHandler, videos}) => {
-    
-//     return(
-//         <div className='videolist__section'>
-//         <p className='videolist__header'>NEXT VIDEOS</p>
-//         { Data.map(videos => {
-//             return(
-//                 <div className='videolist__container' onClick={() => clickHandler(videos.id)}>
-//                     <div>
-//                         <img src={videos.image} alt="Thumbnail of each video in the video list section." className='videolist__video' />
-//                     </div>
-//                     <div className='videolist__text'>
-//                         <p className='videolist__title'>{videos.title}</p>
-//                         <p className='videolist__channel'>{videos.channel}</p>
-//                     </div>
-//                 </div>
-//             )
-//         }
-//         )}
-//         </div>
-//     )
-// }
-
-// export default VideoList;
